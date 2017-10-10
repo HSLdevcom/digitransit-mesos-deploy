e7fd835a796d18918281698f8d51c22e7b1ad7f4
Joel Lappalainen <joel.lappalainen@cgi.com>
2017-10-10 12:24:19 +0300

    Config parser script

---
+++ scripts/parse_config.py
@@ -0,0 +1,23 @@
+import json
+import requests
+import sys
+
+# run on some node in the network
+# works with python3
+# just give service name as an argument when you run the script
+
+def main(service):
+    req = requests.get('http://leader.mesos:8080/v2/apps/' + service, headers = {'Accept': 'application/json'}).text
+    data = json.loads(req)['app']
+    removeFields = ['tasks','lastTaskFailure','versionInfo','version','deployments',
+        'uris','fetch','executor','tasksStaged','tasksRunning','tasksHealthy','tasksRunning',
+        'tasksUnhealthy','ipAddress','residency','secrets','requirePorts','user','args',
+        'storeUrls','constraints']
+    for field in removeFields:
+        if field in data:
+            del data[field]
+    print(json.dumps(data, sort_keys=True, indent=4, separators=(',', ': ')))
+
+if __name__ == "__main__":
+    main(sys.argv[1])
+
