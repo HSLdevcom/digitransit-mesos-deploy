a737e14efb704bcb882dd679007d430a99285a65
Joel Lappalainen <joel.lappalainen@cgi.com>
2017-12-15 16:31:56 +0200

    refactor script to translate server hostname to azure portal instance name

--- scripts/hostname_to_azurename.py
+++ scripts/hostname_to_azurename.py
@@ -8,19 +8,12 @@ import sys
 
 def main(hostname):
     hostname_backwards = str(hostname[::-1])
-    total_number = 0
-    i = 0
-    for character in hostname_backwards:
-        if character.isalpha():
-            number = ord(character) - 55
-        else:
-            number = int(character)
-            if number == 0:
-                break
-        if i > 0:
-            total_number += 35
-        total_number += number
-        i += 1
+    total_number = 36 * int(hostname_backwards[1])
+    first_char = hostname_backwards[0]
+    if first_char.isalpha():
+        total_number += ord(first_char) - 55
+    else:
+        total_number += int(first_char)
     print hostname[:26] + "-vmss0_" + str(total_number)
 
 if __name__ == "__main__":
