4e5c903c4bcfa293f21e883444661a52149f5567
Joel Lappalainen <joel.lappalainen@cgi.com>
2019-08-27 12:00:16 +0300

    Support python3 in hostname_to_azurename.py

--- scripts/hostname_to_azurename.py
+++ scripts/hostname_to_azurename.py
@@ -14,7 +14,7 @@ def main(hostname):
         total_number += ord(first_char) - 55
     else:
         total_number += int(first_char)
-    print hostname[:26] + "-vmss0_" + str(total_number)
+    print(hostname[:26] + "-vmss0_" + str(total_number))
 
 if __name__ == "__main__":
    main(sys.argv[1])
